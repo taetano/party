9cb15256bdb2ad72282b3190833da22b02e1b5e1
Chaho Choi <dhfkdhaneod@gmail.com>
2023-02-12 11:51:58 +0900

    #[1] [build] 데이터베이스 연결 관련 설정 및 jpa-config 추가
    - 인텔리제이 환경변수를 통한 데이터베이스 접속 설정을 해주세요.
    
    Related to: #1

---
+++ src/main/java/com/example/party/global/config/JpaConfiguration.java
@@ -0,0 +1,9 @@
+package com.example.party.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
+@EnableJpaRepositories
+public class JpaConfiguration {
+}
