c04bb700047e29678c84586639f104c9873d7e53
SangHwan <skyi0909@naver.com>
2023-02-13 11:41:49 +0900

    #1 [fix]  메소드 이름 지정 , ProfileResponseDto 이름 변경

--- src/main/java/com/example/party/user/dto/ProfileResponseDto.java
+++
@@ -1,4 +0,0 @@
-package com.example.party.user.dto;
-
-public class ProfileResponseDto { // 예시
-}
--- src/main/java/com/example/party/user/service/IUserService.java
+++ src/main/java/com/example/party/user/service/IUserService.java
@@ -2,13 +2,14 @@ package com.example.party.user.service;
 
 import com.example.party.global.dto.DataResponseDto;
 import com.example.party.global.dto.ResponseDto;
-import com.example.party.user.dto.ProfileResponseDto;
+import com.example.party.user.dto.ProfileResponse;
 
 public interface IUserService {
 	ResponseDto signUp();
 	ResponseDto signIn();
 	ResponseDto signOut();
 	ResponseDto withdraw();
-	DataResponseDto<ProfileResponseDto> updateProfile(); // 예시
-	DataResponseDto<ProfileResponseDto> findProfile(); // 상대방 프로필 조회
+	DataResponseDto<ProfileResponse> updateProfile(); // 예시 , 프로필 수정
+	DataResponseDto<ProfileResponse> findProfile(); // 상대방 프로필 조회 , findOtherProfile
+	DataResponseDto<ProfileResponse> findMyProfile(); // 내 프로필 조회
 }
