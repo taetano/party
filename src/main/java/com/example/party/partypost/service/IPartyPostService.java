0bcaf8b705e4110764a724495bdf98131d32f96a
jwoon1013 <jwoon1013@gmail.com>
2023-02-13 12:15:43 +0900

    #1 [build] inteface 메소드명 지정

--- src/main/java/com/example/party/partypost/service/IPartyPostService.java
+++ src/main/java/com/example/party/partypost/service/IPartyPostService.java
@@ -5,8 +5,11 @@ import com.example.party.global.dto.ListResponseDto;
 
 public interface IPartyPostService {
 	DataResponseDto<?> createPartyPost();
-	ListResponseDto<?> findPartyPosts();
-	DataResponseDto<?> findOne();
+	ListResponseDto<?> findPartyPosts(); // 전체모집글 조회
+	DataResponseDto<?> viewPartyPost(); // 개별 모집글 조회
 	DataResponseDto<?> partyPostLike(); //모집게시물 좋아요
+
+	DataResponseDto<?> deletePartyPost(); //모집글 삭제
+
 
 }
