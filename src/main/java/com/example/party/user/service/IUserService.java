13c529d3369cf9b75198b58fc9760f1930d27509
SangHwan <skyi0909@naver.com>
2023-02-13 12:24:14 +0900

    #1 [fix] profile 메소드 재수정

--- src/main/java/com/example/party/user/service/IUserService.java
+++ src/main/java/com/example/party/user/service/IUserService.java
@@ -3,6 +3,7 @@ package com.example.party.user.service;
 import com.example.party.global.dto.DataResponseDto;
 import com.example.party.global.dto.ResponseDto;
 import com.example.party.user.dto.LoginRequest;
+import com.example.party.user.dto.ProfileResponse;
 import com.example.party.user.dto.SignupReqest;
 import com.example.party.user.entity.User;
 
@@ -11,6 +12,7 @@ public interface IUserService {
 	ResponseDto signIn(LoginRequest loginRequest);
 	ResponseDto signOut(User user);
 	ResponseDto withdraw(User user);
-	DataResponseDto<ProfileResponseDto> updateProfile(); // 예시
-	DataResponseDto<ProfileResponseDto> findProfile(); // 상대방 프로필 조회
+	DataResponseDto<ProfileResponse> updateProfile(); // 예시 , 프로필 수정
+	DataResponseDto<ProfileResponse> findProfile(); // 상대방 프로필 조회 , findOtherProfile
+	DataResponseDto<ProfileResponse> findMyProfile(); // 내 프로필 조회
 }
