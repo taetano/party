b7038f3c780c3a6fc254547e7decfd98e2fa95d0
Chaho Choi <dhfkdhaneod@gmail.com>
2023-02-15 11:59:59 +0900

    #27 [Feat] 익셉션 처리 기능 추가
    - 엑셉션 전역처리를 위한 GlobalExceptionHandler 추가
    - 이후 추가될 커스텀익셉션의 베이스익셉션 추가
    - 전역처리에서 반환될 ErrorResponse 추가

---
+++ src/main/java/com/example/party/global/exception/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.example.party.global.exception;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.example.party.global.dto.ErrorResponse;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+	@ExceptionHandler(BaseException.class)
+	public ResponseEntity<ErrorResponse> exceptionHandler(HttpServletRequest request, BaseException e) {
+		return ResponseEntity.status(e.getStatus())
+			.body(new ErrorResponse(
+				e.getStatus().value(),
+				e.getStatus().name(),
+				e.getMessage(),
+				request.getRequestURI()
+			));
+	}
+}
