a49da4bbe05340ba917b3cfea6e05cfe3863d9ed
JinseonYe <aatwe24@gmail.com>
2023-02-23 17:05:52 +0900

    #99 [Fix] 오류 수정 완료

--- src/main/java/com/example/party/restriction/dto/ReportPostResponse.java
+++ src/main/java/com/example/party/restriction/dto/ReportPostResponse.java
@@ -7,11 +7,15 @@ import lombok.Getter;
 
 @Getter
 public class ReportPostResponse {
+	private final Long id; //신고 이벤트의 Id
+	private final Long reportedPostId; //신고된 모집글의 Id
 	private final String postTitle;
 	private final ReportReason response;
 	private final String detailReason;
 
 	public ReportPostResponse(ReportPost reportPost) {
+		this.id = reportPost.getId();
+		this.reportedPostId = reportPost.getReportPost().getId();
 		this.postTitle = reportPost.getReportPost().getTitle();
 		this.response = reportPost.getReason();
 		this.detailReason = reportPost.getDetailReason();
