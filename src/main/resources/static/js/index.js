1846fdf7bb228663800b8fec694b706bbf16f628
Hwan <76806770+sang-hwann@users.noreply.github.com>
2023-02-25 18:36:43 +0900

    [feature] index페이지 hot모집글기능연결, near모집글 기능 연결(유저현주소값 관련문제로 일부만 구현)

---
+++ src/main/resources/static/js/index.js
@@ -0,0 +1,127 @@
+jQuery(document).ready(function ($) {
+    getHotPartyPost();
+    getNearPartyPost();
+})
+
+function getCookieValue(cookieName) {
+    var name = cookieName + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var cookieParts = decodedCookie.split(';');
+    for (var i = 0; i < cookieParts.length; i++) {
+        var cookiePart = cookieParts[i].trim();
+        if (cookiePart.indexOf(name) === 0) {
+            return cookiePart.substring(name.length, cookiePart.length);
+        }
+    }
+    return "";
+}
+
+function getHotPartyPost() {
+    $('#hotPartyposts').empty()
+
+    $.ajax({
+        url: "http://localhost:8080/api/party-posts/hot",
+        headers: {
+            "Authorization": getCookieValue('Authorization')
+        },
+        type: "GET",
+        success: function (response) {
+            let responseData = response['data']
+            for (let i = 0; i < responseData.length; i++) {
+                let obj = responseData[i];
+                let postId = obj['postId']
+                let title = obj['title']
+                let partyOwner = obj['partyOwner']
+                let status = obj['status']
+                let acceptedMember = obj['acceptedMember']
+                let maxMember = obj['maxMember']
+                let partyDate = new Date(obj['partyDate'])
+                let closeDate = new Date(obj['closeDate'])
+                let partyAddress = obj['partyAddress']
+                let partyPlace = obj['partyPlace']
+                let tempHtml = `
+        <div class="col-lg-4 my-5">
+            <div class="card h-100 shadow border-0">
+                <div class="card-body p-4">
+                    <div class="badge bg-primary bg-gradient rounded-pill mb-2">모집상태 :${status}</div>
+                    <a class="text-decoration-none link-dark stretched-link" href="#!"><h5 class="card-title mb-3">${title}</h5></a>
+                    <p class="card-text mb-0">위치 정보: ${partyAddress} / ${partyPlace}</p>
+                    <p class="card-text mb-0">모임일: ${partyDate}</p>
+                    <p class="card-text mb-0">모집 인원 ${acceptedMember}/${maxMember}</p>
+                </div>
+                <div class="card-footer p-4 pt-0 bg-transparent border-top-0">
+                    <div class="d-flex align-items-end justify-content-between">
+                        <div class="d-flex align-items-center">
+                            <img class="rounded-circle me-3" src="https://dummyimage.com/40x40/ced4da/6c757d"
+                                 alt="..."/>
+                            <div class="small">
+                                <div class="fw-bold">파티장 : ${partyOwner}</div>
+                                <div class="text-muted">마감일: ${closeDate}</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+          `
+                $('#hotPartyposts').append(tempHtml)
+            }
+        }
+    });
+}
+
+function getNearPartyPost() {
+    $('#nearPartyposts').empty()
+
+    let Address = "서울시 마포구 연남동"; //임시로 주소 고정 입력
+    $.ajax({
+        url: "http://localhost:8080/api/party-posts/near/" + Address,
+        headers: {
+            "Authorization": getCookieValue('Authorization')
+        },
+        type: "GET",
+        contentType: "application/json; charset=UTF-8",
+        success: function (response) {
+            let responseData = response['data']
+            for (let i = 0; i < responseData.length; i++) {
+                let obj = responseData[i];
+                let postId = obj['postId']
+                let title = obj['title']
+                let partyOwner = obj['partyOwner']
+                let status = obj['status']
+                let acceptedMember = obj['acceptedMember']
+                let maxMember = obj['maxMember']
+                let partyDate = new Date(obj['partyDate'])
+                let closeDate = new Date(obj['closeDate'])
+                let partyAddress = obj['partyAddress']
+                let partyPlace = obj['partyPlace']
+                let tempHtml = `
+        <div class="col-lg-4 my-5">
+            <div class="card h-100 shadow border-0">
+                <div class="card-body p-4">
+                    <div class="badge bg-primary bg-gradient rounded-pill mb-2">모집상태 :${status}</div>
+                    <a class="text-decoration-none link-dark stretched-link" href="#!"><h5 class="card-title mb-3">${title}</h5></a>
+                    <p class="card-text mb-0">위치 정보: ${partyAddress} / ${partyPlace}</p>
+                    <p class="card-text mb-0">모임일: ${partyDate}</p>
+                    <p class="card-text mb-0">모집 인원 ${acceptedMember}/${maxMember}</p>
+                </div>
+                <div class="card-footer p-4 pt-0 bg-transparent border-top-0">
+                    <div class="d-flex align-items-end justify-content-between">
+                        <div class="d-flex align-items-center">
+                            <img class="rounded-circle me-3" src="https://dummyimage.com/40x40/ced4da/6c757d"
+                                 alt="..."/>
+                            <div class="small">
+                                <div class="fw-bold">파티장 : ${partyOwner}</div>
+                                <div class="text-muted">마감일: ${closeDate}</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+          `
+                $('#nearPartyposts').append(tempHtml)
+            }
+        }
+    });
+}
