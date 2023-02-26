ff976b8dcf9ff2f36a9d9ffceadbccbbc122100e
Chaho Choi <dhfkdhaneod@gmail.com>
2023-02-26 17:38:02 +0900

    #118 [Test] Application 서비스 테스트 코드 작성

--- src/test/java/com/example/party/application/service/ApplicationServiceTest.java
+++ src/test/java/com/example/party/application/service/ApplicationServiceTest.java
@@ -1,232 +1,336 @@
-//package com.example.party.application.service;
-//
-//import static com.example.party.fixture.ApplicationFixture.*;
-////import static com.example.party.fixture.PartyPostFixture.*;
-////import static com.example.party.fixture.UserFixture.*;
-//import static org.assertj.core.api.Assertions.*;
-//import static org.mockito.BDDMockito.*;
-//
-//import java.util.Optional;
-//
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Nested;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.mockito.InjectMocks;
-//import org.mockito.Mock;
-//import org.mockito.junit.jupiter.MockitoExtension;
-//import org.springframework.data.domain.Page;
-//import org.springframework.data.domain.Pageable;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.web.server.ResponseStatusException;
-//
-//import com.example.party.application.dto.ApplicationResponse;
-//import com.example.party.application.entity.Application;
-//import com.example.party.application.repository.ApplicationRepository;
-//import com.example.party.application.type.ApplicationStatus;
-//import com.example.party.global.common.ApiResponse;
-//import com.example.party.global.common.DataApiResponse;
-//import com.example.party.partypost.entity.PartyPost;
-//import com.example.party.partypost.repository.PartyPostRepository;
-//import com.example.party.user.entity.User;
-//
-//@ExtendWith(MockitoExtension.class)
-//class ApplicationServiceTest {
-//
-//	@Mock
-//	private ApplicationRepository applicationRepository;
-//	@Mock
-//	private PartyPostRepository partyPostRepository;
-//	@Mock
-//	private User user;
-//	@InjectMocks
-//	private ApplicationService applicationService;
-//
-//	@DisplayName("성공")
-//	@Nested
-//	class Success {
-//
-//		@Test
-//		void cancelApplication() {
-//			//  given
-//			Application application = application1();
-//
-//			given(applicationRepository.findById(anyLong()))
-//				.willReturn(Optional.of(application));
-//
-//			//  when
-//			ApiResponse result = applicationService.cancelApplication(application.getId(), applicants());
-//
-//			//  then
-//			verify(applicationRepository).findById(anyLong());
-//			assertThat(application.isCancel()).isTrue();
-//			assertThat(result.getCode()).isEqualTo(200);
-//		}
-//
-//		@Test
-//		void getApplications() {
-//			//  given
-//			PartyPost partyPost = partyPost();
-//			Page<Application> applications = applications();
-//			given(partyPostRepository.findById(anyLong()))
-//				.willReturn(Optional.of(partyPost));
-//			given(applicationRepository.findAllByPartyPostAndCancelIsFalse(any(PartyPost.class), any(Pageable.class)))
-//				.willReturn(applications);
-//
-//			//  when
-//			DataApiResponse<ApplicationResponse> ret =
-//				applicationService.getApplications(partyPost.getId(), writer());
-//
-//			//  then
-//			verify(partyPostRepository).findById(anyLong());
-//			assertThat(ret.getData().size()).isEqualTo(2);
-//		}
-//
-//		@Test
-//		void acceptApplication() {
-//			//  given
-//			Application application = application1();
-//
-//			given(applicationRepository.findById(anyLong()))
-//				.willReturn(Optional.of(application));
-//
-//			//  when
-//			assertThat(application.getStatus()).isEqualTo(ApplicationStatus.PENDING);
-//			applicationService.acceptApplication(application.getId(), writer());
-//
-//			//  then
-//			verify(applicationRepository).findById(anyLong());
-//			assertThat(application.getStatus()).isEqualTo(ApplicationStatus.ACCEPT);
-//		}
-//
-//		@Test
-//		void rejectApplication() {
-//			//  given
-//			Application application = application1();
-//
-//			given(applicationRepository.findById(anyLong()))
-//				.willReturn(Optional.of(application));
-//
-//			//  when
-//			assertThat(application.getStatus()).isEqualTo(ApplicationStatus.PENDING);
-//			applicationService.rejectApplication(application.getId(), writer());
-//
-//			//  then
-//			verify(applicationRepository).findById(anyLong());
-//			assertThat(application.getStatus()).isEqualTo(ApplicationStatus.REJECT);
-//		}
-//	}
-//
-//	@DisplayName("실패")
-//	@Nested
-//	class Fail {
-//		@Test
-//		void cancelApplication_FORBIDDEN() {
-//			//  given
-//			Application application = application1();
-//
-//			given(applicationRepository.findById(anyLong()))
-//				.willReturn(Optional.of(application));
-//
-//			//  when
-//			var thrown = assertThatThrownBy(() -> {
-//				applicationService.cancelApplication(application.getId(), writer());
-//			});
-//
-//			//  then
-//			thrown.isInstanceOf(ResponseStatusException.class)
-//				.hasMessageContaining("권한이 없습니다.");
-//			verify(applicationRepository).findById(anyLong());
-//		}
-//
-//		@Test
-//		void getApplications_FORBIDDEN() {
-//			//  given
-//			PartyPost partyPost = partyPost();
-//			given(partyPostRepository.findById(anyLong()))
-//				.willReturn(Optional.of(partyPost));
-//
-//			//  when
-//			var thrown = assertThatThrownBy(() -> {
-//				applicationService.getApplications(partyPost.getId(), applicants());
-//			});
-//
-//			//  then
-//			thrown.isInstanceOf(ResponseStatusException.class)
-//				.hasMessageContaining("권한이 없습니다.");
-//			verify(partyPostRepository).findById(anyLong());
-//		}
-//
-//		@Test
-//		void getApplications_PARTY_POST_NOT_FOUND() {
-//			//  given
-//			Long wrongPartyPostId = 9999L;
-//			given(partyPostRepository.findById(anyLong()))
-//				.willThrow(new ResponseStatusException(HttpStatus.BAD_REQUEST, "NOT FOUND"));
-//
-//			//  when
-//			var thrown = assertThatThrownBy(() -> {
-//				applicationService.getApplications(wrongPartyPostId, applicants());
-//			});
-//
-//			//  then
-//			thrown.isInstanceOf(ResponseStatusException.class)
-//				.hasMessageContaining("NOT FOUND");
-//			verify(partyPostRepository).findById(anyLong());
-//		}
-//
-//		@Test
-//		void acceptApplication_FORBIDDEN() {
-//			//  given
-//			Application application = application1();
-//
-//			given(applicationRepository.findById(anyLong()))
-//				.willReturn(Optional.of(application));
-//			//  when
-//			var thrown = assertThatThrownBy(() -> {
-//				applicationService.acceptApplication(application.getId(), applicants());
-//			});
-//
-//			//  then
-//			thrown.isInstanceOf(ResponseStatusException.class)
-//				.hasMessageContaining("권한이 없습니다.");
-//			verify(applicationRepository).findById(anyLong());
-//		}
-//
-//		@Test
-//		void rejectApplication_FORBIDDEN() {
-//			//  given
-//			Application application = application1();
-//			given(applicationRepository.findById(anyLong()))
-//				.willReturn(Optional.of(application));
-//
-//			//  when
-//			var thrown = assertThatThrownBy(() -> {
-//				applicationService.rejectApplication(application.getId(), applicants());
-//			});
-//
-//			//  then
-//			thrown.isInstanceOf(ResponseStatusException.class)
-//				.hasMessageContaining("권한이 없습니다.");
-//			verify(applicationRepository).findById(anyLong());
-//		}
-//
-//		@Test
-//		@DisplayName("모집글 생성 성공")
-//		void createApplication() {
-//			// //given
-//			// PartyPost partyPost = new PartyPost(user, "테스트", "테스트", (byte)5, "평촌동", "경기도 안양시 평촌동",
-//			// 	"동안로 157 엽기떡볶이",
-//			// 	LocalDateTime.of(2023, Month.FEBRUARY, 20, 13, 15));
-//			//
-//			// when(partyPostRepository.findById(anyLong())).thenReturn(Optional.of(partyPost));
-//			//
-//			// //when
-//			// applicationService.createApplication(1L, user);
-//			// //then
-//			// verify(applicationRepository, times(1));
-//		}
-//
-//	}
-//
-//}
+package com.example.party.application.service;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.BDDMockito.*;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import com.example.party.application.dto.ApplicationResponse;
+import com.example.party.application.entity.Application;
+import com.example.party.application.exception.ApplicationNotFoundException;
+import com.example.party.application.repository.ApplicationRepository;
+import com.example.party.global.common.ApiResponse;
+import com.example.party.global.common.DataApiResponse;
+import com.example.party.global.exception.ForbiddenException;
+import com.example.party.partypost.entity.Party;
+import com.example.party.partypost.entity.PartyPost;
+import com.example.party.partypost.exception.PartyPostNotFoundException;
+import com.example.party.partypost.repository.PartyPostRepository;
+import com.example.party.partypost.repository.PartyRepository;
+import com.example.party.user.entity.User;
+import com.example.party.user.repository.UserRepository;
+
+// TODO: 2023/02/26  
+// 주최자와 신청참가자로 테스트 출력을 나누는게 좋을 듯.
+// 테스트명 naming convention 찾아보기 should, test  
+@ExtendWith({MockitoExtension.class})
+class ApplicationServiceTest {
+
+	@Mock
+	private ApplicationRepository applicationRepository;
+	@Mock
+	private PartyPostRepository partyPostRepository;
+	@Mock
+	private PartyRepository partyRepository;
+	@Mock
+	private UserRepository userRepository;
+	@Mock
+	private ApplicationValidator applicationValidator;
+	@InjectMocks
+	private ApplicationService applicationService;
+
+	private Application application;
+	private User user;
+	private PartyPost partyPost;
+
+	@BeforeEach
+	public void setUp() {
+		this.application = mock(Application.class);
+		this.user = mock(User.class);
+		this.partyPost = mock(PartyPost.class);
+	}
+
+	@DisplayName("성공")
+	@Nested
+	class Success {
+		@Test
+		@DisplayName("모집글에 참가신청을 한다")
+		void createApplication() {
+			//  given
+
+			//  when
+			when(userRepository.save(any(User.class))).thenReturn(user);
+			when(partyPostRepository.findById(anyLong())).thenReturn(Optional.of(partyPost));
+			doNothing().when(applicationValidator)
+				.validationApplicationBeforeCreation(any(PartyPost.class), any(User.class));
+			when(applicationRepository.save(any(Application.class))).thenReturn(application);
+
+			ApiResponse result = applicationService.createApplication(partyPost.getId(), user);
+			//  then
+			verify(userRepository).save(any(User.class));
+			verify(partyPostRepository).findById(anyLong());
+			verify(applicationValidator).validationApplicationBeforeCreation(any(PartyPost.class), any(User.class));
+			verify(applicationRepository).save(any(Application.class));
+			verify(partyPost).addApplication(any(Application.class));
+			verify(user).addApplication(any(Application.class));
+			assertThat(result.getCode()).isEqualTo(200);
+			assertThat(result.getMsg()).isEqualTo("참가 신청 완료");
+		}
+
+		@Test
+		@DisplayName("모집글에 지원한 참가신청을 취소 한다")
+		void cancelApplication() {
+			//  given
+			//  when
+			when(applicationRepository.findById(anyLong())).thenReturn(Optional.of(application));
+			when(application.isWrittenByMe(anyLong())).thenReturn(true);
+
+			ApiResponse result = applicationService.cancelApplication(application.getId(), user);
+
+			//  then
+			verify(applicationRepository).findById(anyLong());
+			verify(application).isWrittenByMe(anyLong());
+			verify(application).cancel();
+			assertThat(result.getCode()).isEqualTo(200);
+			assertThat(result.getMsg()).isEqualTo("참가 신청 취소 완료");
+		}
+
+		@Test
+		@DisplayName("내가 작성한 모집글에 신청한 참가신청서들을 조회 한다")
+		void getApplications() {
+			//  given
+			Page<Application> applications = mock(Page.class);
+
+			//  when
+			when(partyPostRepository.findById(anyLong())).thenReturn(Optional.of(partyPost));
+			when(partyPost.isWrittenByMe(anyLong())).thenReturn(true);
+			when(applicationRepository.findAllByPartyPostAndCancelIsFalse(any(PartyPost.class), any(Pageable.class)))
+				.thenReturn(applications);
+
+			DataApiResponse<ApplicationResponse> result = applicationService.getApplications(partyPost.getId(),
+				user);
+
+			//  then
+			verify(partyPostRepository).findById(anyLong());
+			verify(partyPost).isWrittenByMe(anyLong());
+			verify(applicationRepository).findAllByPartyPostAndCancelIsFalse(any(PartyPost.class), any(Pageable.class));
+			assertThat(result.getCode()).isEqualTo(200);
+			assertThat(result.getMsg()).isEqualTo("참가신청자 목록 조회 완료");
+			assertThat(result.getData()).isEmpty();
+		}
+
+		@Test
+		@DisplayName("내가 작성한 모집글에 신청한 하나의 참가신청서를 참가수락한다")
+		void acceptApplication() {
+			//  given
+			Party party = mock(Party.class);
+
+			//  when
+			when(applicationRepository.findById(anyLong())).thenReturn(Optional.of(application));
+			when(application.isSendToMe(anyLong())).thenReturn(true);
+			doNothing().when(applicationValidator).validateApplicationStatus(any(Application.class));
+			when(partyRepository.findByPartyPostId(anyLong())).thenReturn(Optional.of(party));
+			when(application.getPartyPost()).thenReturn(partyPost);
+			when(partyPost.getId()).thenReturn(1L);
+			when(application.getUser()).thenReturn(user);
+
+			ApiResponse result = applicationService.acceptApplication(application.getId(), user);
+
+			//  then
+			verify(applicationRepository).findById(anyLong());
+			verify(application).accept();
+			verify(partyRepository).findByPartyPostId(anyLong());
+			verify(party).autoAddUser(any(User.class));
+			verify(partyRepository).save(any(Party.class));
+			assertThat(result.getCode()).isEqualTo(200);
+			assertThat(result.getMsg()).isEqualTo("참가 신청 수락 완료");
+		}
+
+		@Test
+		@DisplayName("내가 작성한 모집글에 신청한 하나의 참가신청서를 참가거부한다")
+		void rejectApplication() {
+			//  given
+
+			//  when
+			when(applicationRepository.findById(anyLong())).thenReturn(Optional.of(application));
+			when(application.isSendToMe(anyLong())).thenReturn(true);
+			doNothing().when(applicationValidator).validateApplicationStatus(any(Application.class));
+
+			ApiResponse result = applicationService.rejectApplication(application.getId(), user);
+
+			//  then
+			verify(applicationRepository).findById(anyLong());
+			verify(application).isSendToMe(anyLong());
+			verify(application).reject();
+			assertThat(result.getCode()).isEqualTo(200);
+			assertThat(result.getMsg()).isEqualTo("참가 신청 거부 완료");
+		}
+
+		@Test
+		@DisplayName("내가 작성한 모집글에 신청한 하나의 참가신청서를 조회한다")
+		void getApplication() {
+			//  given
+
+			//  when
+			when(applicationRepository.findById(anyLong())).thenReturn(Optional.of(application));
+
+			applicationService.getApplication(999L);
+			//  then
+			verify(applicationRepository).findById(anyLong());
+		}
+	}
+
+	@DisplayName("실패")
+	@Nested
+	class Fail {
+		@Test
+		void createApplication_PartyPostNotFoundException() {
+		//  given
+			
+		//  when
+			when(userRepository.save(any(User.class))).thenReturn(user);
+
+			var thrown = assertThatThrownBy(() ->
+				applicationService.createApplication(partyPost.getId(), user)
+			);
+			//  then
+			verify(userRepository).save(any(User.class));
+			thrown.isInstanceOf(PartyPostNotFoundException.class)
+				.hasMessageContaining(PartyPostNotFoundException.MSG);
+		}
+		
+		@Test
+		@DisplayName("내가 신청하지 않은 신청서는 취소할 수 없다")
+		void cancelApplication_FORBIDDEN() {
+			//  given
+			//  when
+			when(applicationRepository.findById(anyLong())).thenReturn(Optional.of(application));
+			when(application.isWrittenByMe(anyLong())).thenReturn(false);
+
+			var thrown = assertThatThrownBy(() -> {
+				applicationService.cancelApplication(application.getId(), user);
+			});
+
+			//  then
+			thrown.isInstanceOf(ForbiddenException.class)
+				.hasMessageContaining("허가되지 않은 요청입니다.");
+			verify(applicationRepository).findById(anyLong());
+		}
+
+		@Test
+		void getApplications_PartyPostNotFoundException() {
+			//  given
+
+			//  when
+			var thrown = assertThatThrownBy(() -> {
+				applicationService.getApplications(partyPost.getId(), user);
+			});
+
+			//  then
+			thrown.isInstanceOf(PartyPostNotFoundException.class)
+				.hasMessageContaining( "존재하지 않는 모집글 입니다.");
+			verify(partyPostRepository).findById(anyLong());
+		}
+
+		@Test
+		void getApplications_FORBIDDEN() {
+			//  given
+			//  when
+			when(partyPostRepository.findById(anyLong())).thenReturn(Optional.of(partyPost));
+
+			var thrown = assertThatThrownBy(() -> {
+				applicationService.getApplications(partyPost.getId(), user);
+			});
+
+			//  then
+			thrown.isInstanceOf(ForbiddenException.class)
+				.hasMessageContaining("허가되지 않은 요청입니다.");
+			verify(partyPostRepository).findById(anyLong());
+			verify(partyPost).isWrittenByMe(anyLong());
+		}
+
+		@Test
+		void acceptApplication_FORBIDDEN() {
+			//  given
+			//  when
+			when(applicationRepository.findById(anyLong())).thenReturn(Optional.of(application));
+
+			var thrown = assertThatThrownBy(() -> {
+				applicationService.acceptApplication(partyPost.getId(), user);
+			});
+
+			//  then
+			thrown.isInstanceOf(ForbiddenException.class)
+				.hasMessageContaining("허가되지 않은 요청입니다.");
+			verify(applicationRepository).findById(anyLong());
+			verify(application).isSendToMe(anyLong());
+		}
+
+		@Test
+		void acceptApplication_PartyPostNotFoundException() {
+			//  given
+			//  when
+			when(applicationRepository.findById(anyLong())).thenReturn(Optional.of(application));
+			when(application.isSendToMe(anyLong())).thenReturn(true);
+			doNothing().when(applicationValidator).validateApplicationStatus(application);
+			when(application.getPartyPost()).thenReturn(partyPost);
+			when(partyPost.getId()).thenReturn(1L);
+
+			var thrown = assertThatThrownBy(() -> {
+				applicationService.acceptApplication(partyPost.getId(), user);
+			});
+
+			//  then
+			thrown.isInstanceOf(PartyPostNotFoundException.class)
+				.hasMessageContaining( "존재하지 않는 모집글 입니다.");
+			verify(applicationRepository).findById(anyLong());
+			verify(application).isSendToMe(anyLong());
+			verify(applicationValidator).validateApplicationStatus(any(Application.class));
+			verify(application).accept();
+			verify(partyRepository).findByPartyPostId(anyLong());
+		}
+
+		@Test
+		void rejectApplication_FORBIDDEN() {
+			//  given
+			//  when
+			when(applicationRepository.findById(anyLong())).thenReturn(Optional.of(application));
+
+			var thrown = assertThatThrownBy(() -> {
+				applicationService.rejectApplication(application.getId(), user);
+			});
+
+			//  then
+			thrown.isInstanceOf(ForbiddenException.class)
+				.hasMessageContaining("허가되지 않은 요청입니다.");
+			verify(applicationRepository).findById(anyLong());
+		}
+
+		@Test
+		void getApplication_ApplicationNotFoundException() {
+		//  given
+		//  when
+			var thrown = assertThatThrownBy(() -> {
+				applicationService.getApplication(anyLong());
+			});
+			//  then
+
+			thrown.isInstanceOf(ApplicationNotFoundException.class)
+				.hasMessageContaining(ApplicationNotFoundException.MSG);
+			verify(applicationRepository).findById(anyLong());
+		}
+
+	}
+
+}
