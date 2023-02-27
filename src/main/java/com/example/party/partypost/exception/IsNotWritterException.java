f1d3e676bf8897d7e7308c6db5a2294dbdb16673
Chaho Choi <dhfkdhaneod@gmail.com>
2023-02-27 11:00:48 +0900

    #120 [Test] PartyPostService Throw Exception 추가

--- src/main/java/com/example/party/partypost/exception/IsNotWritterException.java
+++ src/main/java/com/example/party/partypost/exception/IsNotWritterException.java
@@ -5,9 +5,9 @@ import org.springframework.http.HttpStatus;
 import com.example.party.global.exception.BaseException;
 
 public class IsNotWritterException extends BaseException {
-	private static final String msg = "해당 회원이 작성한 글이 아닙니다.";
+	public static final String MSG = "해당 회원이 작성한 글이 아닙니다.";
 
 	public IsNotWritterException() {
-		super(HttpStatus.FORBIDDEN, msg);
+		super(HttpStatus.FORBIDDEN, MSG);
 	}
 }
