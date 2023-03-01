f2522a20e96626e969f10795aa2b56a91b7bfd00
Chaho Choi <dhfkdhaneod@gmail.com>
2023-03-01 15:13:53 +0900

    #130 [Test] UserController test 추가

---
+++ src/test/java/com/example/party/user/UserTestHelper.java
@@ -0,0 +1,37 @@
+package com.example.party.user;
+
+import com.example.party.user.dto.LoginRequest;
+import com.example.party.user.dto.ProfilesRequest;
+import com.example.party.user.dto.SignupRequest;
+import com.example.party.user.dto.WithdrawRequest;
+
+public class UserTestHelper {
+	public static SignupRequest signupRequest() {
+		return new SignupRequest(
+			"test@email.com",
+			"password1!",
+			"nickname",
+			"010-1234-1234"
+		);
+	}
+
+	public static LoginRequest loginRequest() {
+		return new LoginRequest(
+			"test@eamil.com",
+			"password1!"
+		);
+	}
+
+	public static WithdrawRequest withdrawRequest() {
+		return new WithdrawRequest("password1!");
+	}
+
+	public static ProfilesRequest profilesRequest() {
+		return new ProfilesRequest(
+			"nickname",
+			"010-1234-1234",
+			"",
+			"comment"
+		);
+	}
+}
