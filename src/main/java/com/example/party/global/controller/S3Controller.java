9716dac0839e9b2ec50b6a6305581149ce0e88de
JinseonYe <aatwe24@gmail.com>
2023-03-03 23:28:14 +0900

    #151 [Feat] aws s3 연결 작업 테스트 완료

---
+++ src/main/java/com/example/party/global/controller/S3Controller.java
@@ -0,0 +1,30 @@
+package com.example.party.global.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+import com.example.party.global.util.S3Uploader;
+
+@RequiredArgsConstructor
+@Controller
+public class S3Controller {
+	private final S3Uploader s3Uploader;
+
+	@GetMapping("/test")
+	public String index() {
+		return "test";
+	}
+
+	@PostMapping("/upload")
+	@ResponseBody
+	public String upload(@RequestParam("data") MultipartFile multipartFile) throws IOException {
+		return s3Uploader.upload(multipartFile, "static");
+	}
+}
