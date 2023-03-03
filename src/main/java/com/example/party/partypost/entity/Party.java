b80c32f0a1f2100cef97f62ff19213ffaed8720a
Hwan <76806770+sang-hwann@users.noreply.github.com>
2023-03-03 12:16:36 +0900

    [fix] party연관관계수정

--- src/main/java/com/example/party/partypost/entity/Party.java
+++ src/main/java/com/example/party/partypost/entity/Party.java
@@ -3,7 +3,13 @@ package com.example.party.partypost.entity;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 
 import com.example.party.user.entity.User;
 
@@ -24,10 +30,10 @@ public class Party {
 
 	//브릿지 테이블
 	@OneToMany()
-	private List<User> users;
+	@JoinColumn(name = "party_id")
+	private List<User> users = new ArrayList<>();
 
 	public Party(PartyPost partyPost) {
-		this.users = new ArrayList<>();
 		this.partyPost = partyPost;
 		autoAddUser(partyPost.getUser());
 	}
@@ -39,6 +45,7 @@ public class Party {
 	public void autoAddUser(User user) {
 		this.users.add(user);
 	}
+
 	public void addUser(User user) {
 		this.users.add(user);
 	}
