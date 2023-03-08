7bd832b7a1244ddfad151900c16dd1cbfb022b7a
jwoon1013 <jwoon1013@gmail.com>
2023-03-08 19:43:28 +0900

    #218 [fix] 민감 코드 가리는 방법 수정
    - ec2에 원본 파일 올려두고, 실행시 참조하는 방식으로 변경

--- scripts/start.sh
+++ scripts/start.sh
@@ -9,16 +9,13 @@ DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
 TIME_NOW=$(date +%c)
 
-# 환경변수 읽도록 추가 코드인데, 현재 동작하지 않아 잠시 주석처리 했습니다.
-#source ~/.bashrc
-
 # build 파일 복사
 echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
 cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
 
 # jar 파일 실행
 echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
-nohup java -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
+nohup java -jar $JAR_FILE --spring.config.location=./application.yml > $APP_LOG 2> $ERROR_LOG &
 
 CURRENT_PID=$(pgrep -f $JAR_FILE)
 echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
