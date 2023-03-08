cddeccf9cbd100984f6e72497380bf986f6ea999
Hwan <76806770+sang-hwann@users.noreply.github.com>
2023-03-08 19:41:28 +0900

    #236 [bug] 카테고리 검색후 페이지 이동시 문제 수정

--- src/main/resources/static/js/search-result.js
+++ src/main/resources/static/js/search-result.js
@@ -107,6 +107,25 @@ function firstsearchPartyPost(inputVal) {
 
                 $('#searchPartyPostResult').append(tempHtml)
 
+                $('#pageNavigation').empty();
+
+                tempHtml = `<nav aria-label="Page navigation example">
+                                    <ul>
+                                        <li><a href="#" onclick="searchPartyPost(1)">1</a></li>
+                                        <li><a href="#" onclick="searchPartyPost(2)">2</a></li>
+                                        <li><a href="#" onclick="searchPartyPost(3)">3</a></li>
+                                        <li><a href="#" onclick="searchPartyPost(4)">4</a></li>
+                                        <li><a href="#" onclick="searchPartyPost(5)">5</a></li>
+                                        <li><a href="#" onclick="searchPartyPost(6)">6</a></li>
+                                        <li><a href="#" onclick="searchPartyPost(7)">7</a></li>
+                                        <li><a href="#" onclick="searchPartyPost(8)">8</a></li>
+                                        <li><a href="#" onclick="searchPartyPost(9)">9</a></li>
+                                        <li><a href="#" onclick="searchPartyPost(10)">10</a></li>
+                                    </ul>
+                                </nav>`
+
+                $('#pageNavigation').append(tempHtml);
+
             }
         }
     });
@@ -173,6 +192,25 @@ function searchPartyPost(page) {
 
                 $('#searchPartyPostResult').append(tempHtml)
 
+                $('#pageNavigation').empty();
+
+                tempHtml = `<nav aria-label="Page navigation example">
+                                    <ul>
+                                        <li><a href="#" onclick="searchPartyPost(1)">1</a></li>
+                                        <li><a href="#" onclick="searchPartyPost(2)">2</a></li>
+                                        <li><a href="#" onclick="searchPartyPost(3)">3</a></li>
+                                        <li><a href="#" onclick="searchPartyPost(4)">4</a></li>
+                                        <li><a href="#" onclick="searchPartyPost(5)">5</a></li>
+                                        <li><a href="#" onclick="searchPartyPost(6)">6</a></li>
+                                        <li><a href="#" onclick="searchPartyPost(7)">7</a></li>
+                                        <li><a href="#" onclick="searchPartyPost(8)">8</a></li>
+                                        <li><a href="#" onclick="searchPartyPost(9)">9</a></li>
+                                        <li><a href="#" onclick="searchPartyPost(10)">10</a></li>
+                                    </ul>
+                                </nav>`
+
+                $('#pageNavigation').append(tempHtml);
+
             }
         }
     });
@@ -230,10 +268,107 @@ function searchPartyPostCategory(categoryId) {
 
                 $('#searchPartyPostResult').append(tempHtml)
 
+
+                $('#pageNavigation').empty();
+
+                tempHtml = `<nav aria-label="Page navigation example">
+                                    <ul>
+                                        <li><a href="#" onclick="searchPartyPostCategoryPage(${categoryId},1)">1</a></li>
+                                        <li><a href="#" onclick="searchPartyPostCategoryPage(${categoryId},2)">2</a></li>
+                                        <li><a href="#" onclick="searchPartyPostCategoryPage(${categoryId},3)">3</a></li>
+                                        <li><a href="#" onclick="searchPartyPostCategoryPage(${categoryId},4)">4</a></li>
+                                        <li><a href="#" onclick="searchPartyPostCategoryPage(${categoryId},5)">5</a></li>
+                                        <li><a href="#" onclick="searchPartyPostCategoryPage(${categoryId},6)">6</a></li>
+                                        <li><a href="#" onclick="searchPartyPostCategoryPage(${categoryId},7)">7</a></li>
+                                        <li><a href="#" onclick="searchPartyPostCategoryPage(${categoryId},8)">8</a></li>
+                                        <li><a href="#" onclick="searchPartyPostCategoryPage(${categoryId},9)">9</a></li>
+                                        <li><a href="#" onclick="searchPartyPostCategoryPage(${categoryId},10)">10</a></li>
+                                    </ul>
+                                </nav>`
+
+                $('#pageNavigation').append(tempHtml);
             }
         }
     });
 }
+
+//카테고리 조회 페이지별로 재조회
+function searchPartyPostCategoryPage(categoryId,page) {
+    $('#searchPartyPostResult').empty();
+    $.ajax({
+        url: `/api/party-posts/categories/${categoryId}`,
+        headers: {
+            "Authorization": getCookieValue('Authorization')
+        },
+        data: {
+            page: page
+        },
+        type: "GET",
+        success: function (response) {
+            console.log(response);
+            let responseData = response['data']
+            for (let i = 0; i < responseData.length; i++) {
+                let obj = responseData[i];
+                let postId = obj['postId'];
+                let title = obj['title'];
+                let partyOwner = obj['partyOwner'];
+                let status = obj['status'];
+                let acceptedMember = obj['acceptedMember'];
+                let maxMember = obj['maxMember'];
+                let partyDate = new Date(obj['partyDate']);
+                let closeDate = new Date(obj['closeDate']);
+                let partyAddress = obj['partyAddress'];
+                let partyPlace = obj['partyPlace'];
+                let tempHtml = `
+        <div class="col-lg-4 my-5">
+            <div class="card h-100 shadow border-0">
+                <div class="card-body p-4" onclick="partypostClick(${postId})">
+                    <div class="badge bg-primary bg-gradient rounded-pill mb-2">모집상태 :${status}</div>
+                    <a class="text-decoration-none link-dark stretched-link" href="#!"><h5 class="card-title mb-3">${title}</h5></a>
+                    <p class="card-text mb-0">위치 정보: ${partyAddress} / ${partyPlace}</p>
+                    <p class="card-text mb-0">모임일: ${partyDate}</p>
+                    <p class="card-text mb-0">모집 인원 ${acceptedMember}/${maxMember}</p>
+                </div>
+                <div class="card-footer p-4 pt-0 bg-transparent border-top-0">
+                    <div class="d-flex align-items-end justify-content-between">
+                        <div class="d-flex align-items-center">
+                            <img class="rounded-circle me-3" src="https://dummyimage.com/40x40/ced4da/6c757d"
+                                 alt="..."/>
+                            <div class="small">
+                                <div class="fw-bold">파티장 : ${partyOwner}</div>
+                                <div class="text-muted">마감일: ${closeDate}</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+          `
+
+                $('#searchPartyPostResult').append(tempHtml)
+                $('#pageNavigation').empty();
+
+                tempHtml = `<nav aria-label="Page navigation example">
+                                    <ul>
+                                        <li><a href="#" onclick="searchPartyPostCategoryPage(${categoryId},1)">1</a></li>
+                                        <li><a href="#" onclick="searchPartyPostCategoryPage(${categoryId},2)">2</a></li>
+                                        <li><a href="#" onclick="searchPartyPostCategoryPage(${categoryId},3)">3</a></li>
+                                        <li><a href="#" onclick="searchPartyPostCategoryPage(${categoryId},4)">4</a></li>
+                                        <li><a href="#" onclick="searchPartyPostCategoryPage(${categoryId},5)">5</a></li>
+                                        <li><a href="#" onclick="searchPartyPostCategoryPage(${categoryId},6)">6</a></li>
+                                        <li><a href="#" onclick="searchPartyPostCategoryPage(${categoryId},7)">7</a></li>
+                                        <li><a href="#" onclick="searchPartyPostCategoryPage(${categoryId},8)">8</a></li>
+                                        <li><a href="#" onclick="searchPartyPostCategoryPage(${categoryId},9)">9</a></li>
+                                        <li><a href="#" onclick="searchPartyPostCategoryPage(${categoryId},10)">10</a></li>
+                                    </ul>
+                                </nav>`
+
+                $('#pageNavigation').append(tempHtml);
+            }
+        }
+    });
+}
+
 
 function handleKeyUp(e) {
     if (e.keyCode === 13) {
